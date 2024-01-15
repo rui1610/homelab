36459668285b2ee25c2462ab9d990483a755b89e
Rui Nogueira <rui.nogueira@sap.com>
2024-01-15 21:13:21 +0100

    update debian script for proxmos lxc container

---
+++ proxmox/setup_debian.sh
@@ -0,0 +1,61 @@
+# ----------------------------------------------------------------------------------------------------------------
+# Prepare the debian VM to run docker on LXC
+# (see https://benheater.com/proxmox-run-docker-on-linux-containers-lxc/)
+# ----------------------------------------------------------------------------------------------------------------
+apt clean && apt update
+apt install -y fuse-overlayfs
+ln -s /usr/bin/fuse-overlayfs /usr/local/bin/fuse-overlayfs
+# Ensure pre-requisites are installed
+apt install -y ca-certificates curl gnupg nfs-common sudo nano cifs-utils lsb-release
+
+# ----------------------------------------------------------------------------------------------------------------
+# Install Docker Engine 
+# ----------------------------------------------------------------------------------------------------------------
+# Add Docker GPG key
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Add Docker apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update sources and install Docker Engine
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+# Enable the Docker engine to start at boot
+systemctl enable docker
+systemctl enable containerd
+newgrp docker
+
+# ----------------------------------------------------------------------------------------------------------------
+# Enable ssh to root
+# ----------------------------------------------------------------------------------------------------------------
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# ----------------------------------------------------------------------------------------------------------------
+# Install Terraform (see https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli)
+# ----------------------------------------------------------------------------------------------------------------
+apt update && sudo apt install -y gnupg software-properties-common
+# Install the HashiCorp GPG key.
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# Verify the key's fingerprint.
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+# Add the official HashiCorp repository to your system
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+tee /etc/apt/sources.list.d/hashicorp.list
+# Download the package information from HashiCorp
+apt update
+# Install Terraform from the new repository.
+apt install -y terraform
+
+# ----------------------------------------------------------------------------------------------------------------
+# Give rui user rights to run docker and make him sudo
+# ----------------------------------------------------------------------------------------------------------------
+adduser rui --gecos "" --uid 1000 --gid 1000 --disabled-password
+usermod -aG sudo rui
+echo "rui  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/rui
+
+# don't forget to set the password for user rui! though the command 'passwd rui'  
--- tbd/proxmox/setup_alpine_clx.sh
+++ tbd/proxmox/setup_alpine_clx.sh
@@ -5,26 +5,22 @@
 #apt install -y fuse-overlayfs
 
 # Now on the debian CLX
-apt clean && apt update && apt upgrade -y
-#apt install -y fuse-overlayfs
-#ln -s /usr/bin/fuse-overlayfs /usr/local/bin/fuse-overlayfs
-# Ensure pre-requisites are installed
-apt install -y ca-certificates curl gnupg lsb-release sudo nfs-common cifs-utils
-# Add Docker GPG key
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# Add Docker apt repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-# Update sources and install Docker Engine
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+apk update && apk upgrade
+apk add --update fuse-overlayfs ca-certificates curl gnupg lsb-release sudo cifs-utils nfs-utils 
+
+# Add docker
+apk add --update docker openrc 
+
 # Enable the Docker engine to start at boot
-systemctl enable docker
+rc-update add docker boot
+service docker start
+
+
 
 # ----------------------------------------------------------------------------------------------------------------
 # Now add own user
 # ----------------------------------------------------------------------------------------------------------------
-adduser rui --gecos "" --uid 1000 --gid 100 --disabled-password
+adduser rui --gecos "" --uid 1000 --gid 1000 --disabled-password
 usermod -aG sudo rui
 echo "rui  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/rui
 
