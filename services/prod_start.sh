15eca66dec77e827c8cc5fc6e8e4a9638b46e988
Rui Nogueira <rui.nogueira@sap.com>
2024-01-15 22:26:59 +0000

    move vaultwarden to right folder

--- services/prod_start.sh
+++ services/prod_start.sh
@@ -1,7 +1,7 @@
 docker compose --env-file "../config/docker/variables.env" --env-file "../config/docker/my_credentials.env" -f "homer/prod/docker-compose.yml" up -d
 docker compose --env-file "../config/docker/variables.env" --env-file "../config/docker/my_credentials.env" -f "homer/prod/docker-compose.yml" build
 
-docker compose --env-file "../config/docker/variables.env" --env-file "../config/docker/my_credentials.env" -f "portainer/prod/docker-compose.yml" build
+docker compose --env-file "../../../config/docker/homelab.env" --env-file "../../../config/docker/my_credentials.env" -f "portainer/prod/docker-compose.yml" build
 docker compose --env-file "../config/docker/variables.env" --env-file "../config/docker/my_credentials.env" -f "portainer/prod/docker-compose.yml" up -d
 
 docker compose --env-file "../config/docker/variables.env" --env-file "../config/docker/my_credentials.env" -f "jellyfin/prod/docker-compose.yml" build
