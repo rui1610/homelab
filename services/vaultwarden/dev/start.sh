703ff23e5a04e9b480c7e69d2f20d133535d9432
Rui Nogueira <rui1610@users.noreply.github.com>
2024-01-17 20:16:43 +0100

    Vaultwarden (#7)
    * add vaulwarden + update docker version info
    
    * add script
    
    * clean-up

---
+++ services/vaultwarden/dev/start.sh
@@ -0,0 +1,2 @@
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" build
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" up -d
