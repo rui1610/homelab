ea9c43754a23b62f435d33d5486f31b0ccffcad7
Rui Nogueira <rui.nogueira@sap.com>
2024-08-12 20:49:25 +0200

    remove photoprism

--- services/photoprism/prod/start.sh
+++
@@ -1,2 +0,0 @@
-docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" build
-docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" up -d
