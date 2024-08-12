d7f5e51604b0b9ae1658e541c97cea9b33bac14e
Rui Nogueira <rui.nogueira@sap.com>
2024-08-12 20:19:24 +0200

    add location

--- services/immich/prod/start.sh
+++ services/immich/prod/start.sh
@@ -1,2 +1,2 @@
-docker compose build
-docker compose up -d
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" build
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" up -d
