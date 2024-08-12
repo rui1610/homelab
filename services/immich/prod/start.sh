0c3f4d3fa930c7f3fbaea4f0eae3fb292b78c231
Rui Nogueira <rui.nogueira@sap.com>
2024-08-12 20:22:14 +0200

    add env

--- services/immich/prod/start.sh
+++ services/immich/prod/start.sh
@@ -1,2 +1,2 @@
-docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" build
-docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" up -d
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" --env-file ".env" build
+docker compose --env-file "../../../config/homelab.env" --env-file "../../../config/my_credentials.env" --env-file ".env" up -d
